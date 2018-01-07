91d1171e11cca358c3e541f49109c921c5cf2ece
brantbell <31989675+brantbell@users.noreply.github.com>
2018-01-07 10:21:34 +0800

    Update and rename user-active-list to user-active-list.sh

---
+++ user-active-list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+disable-user-expire
+clear
+
+echo -e "-------------------------\e[032;1mDAFTAR AKUN SSH ACTIVE\e[0m-------------------------"
+echo -e "                       \e[032;1mALL SUPPORTED ZHANGZI-SSL\e[0m"
+echo -e "                 \e[032;1mhttps://www.facebook.com/kopet88\e[0m"
+echo -e "        \e[032;1mCONTACT SUPPORT Telegram @DENBAGUSS SMS/WA 60146309176\e[0m"
+echo ""
+cat /root/activeuser.txt
+echo ""
+
+cd ~/
+
