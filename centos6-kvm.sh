65f54dae7e5b73e5476f6ba7885bd1ccfec48e81
brantbell <31989675+brantbell@users.noreply.github.com>
2018-01-07 20:19:27 +0800

    Update centos6-kvm.sh

--- centos6-kvm.sh
+++ centos6-kvm.sh
@@ -559,6 +559,7 @@ openssl req -new -x509 -key key.pem -out cert.pem -days 1095
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 /etc/init.d/stunnel4 restart
+
 # cron
 service crond start
 chkconfig crond on
