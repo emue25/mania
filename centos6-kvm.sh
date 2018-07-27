571b406b6a46d3ddc9bd2b630dde8ba407f654f6
brantbell <31989675+brantbell@users.noreply.github.com>
2018-07-27 19:19:17 +0800

    Update centos6-kvm.sh

--- centos6-kvm.sh
+++ centos6-kvm.sh
@@ -469,8 +469,8 @@ chkconfig squid on
 cd
 wget "http://script.hostingtermurah.net/repo/webmin-1.801-1.noarch.rpm"
 yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
-rpm -i webmin-1.870-1.noarch.rpm;
-rm webmin-1.870-1.noarch.rpm
+rpm -i webmin-1.801-1.noarch.rpm;
+rm webmin-1.801-1.noarch.rpm
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
 chkconfig webmin on
