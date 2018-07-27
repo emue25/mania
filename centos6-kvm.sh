0c458314c2d9a18e6bcfdfbada8bceb78ff5ee2e
brantbell <31989675+brantbell@users.noreply.github.com>
2018-07-27 21:55:05 +0800

    Update centos6-kvm.sh

--- centos6-kvm.sh
+++ centos6-kvm.sh
@@ -559,7 +559,6 @@ openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 /etc/init.d/stunnel start
-/etc/init.d/stunne restart
 
 # cron
 service crond start
